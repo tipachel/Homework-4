890f83dc2cc6aef0eb4806a4180da9d6113e933e
Boiko Konstantin <92636892+tipachel88@users.noreply.github.com>
2021-10-31 00:10:55 +0300

    Пока ещё ничего не понятно

---
+++ Homework 4/ViewControllers/MoreInfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MoreInfoViewController.swift
+//  Homework 4
+//
+//  Created by Tipachel on 30.10.2021.
+//
+
+import UIKit
+
+class MoreInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+   // MARK: - Navigation
+
+}
